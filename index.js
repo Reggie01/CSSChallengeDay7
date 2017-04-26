79b11ca18206d5b53ec29582cba31327bd95288d
Reggie S <reggie@bgcreatives.com>
2017-04-26 13:51:14 -0400

    Added side bar and main content animations

---
+++ index.js
@@ -0,0 +1,36 @@
+﻿( function() {
+  var containerSlider = document.getElementById( "container-slider-js" );
+  var headerList = document.getElementById( "header-list-js" );
+  var sideBarContainer = document.getElementById( "side-bar-container-js" );
+
+  headerList.addEventListener( "click", headerHandler, false );
+  
+  function headerHandler() {
+    console.log( "Hello World" );
+    
+    var containerSlide = "container-slider";
+    var sideBar = "side-bar-container";
+    
+    containerSlider.className = toggleClassName( containerSlider, containerSlide, "slide-extend", "slide-retract" );
+    
+    sideBarContainer.className = toggleClassName( sideBarContainer, sideBar, "side-container-extend", "side-container-retract" );
+    /* if( classes.indexOf( "slide-extend" ) != -1 ) {
+        containerSlider.className = containerSlide + " slide-retract";
+    } else {
+        containerSlider.className = containerSlide + " slide-extend";
+    } */
+  }
+  
+  function toggleClassName( element, base, current, update ) {
+    var classes = element.className;
+    var updateClass = "";
+    if( classes.indexOf( current ) != -1 ) {
+        updateClass = base + " " + update;
+    } else {
+        updateClass = base + " " + current;
+    }   
+    
+    return updateClass; 
+  }
+  
+})();
